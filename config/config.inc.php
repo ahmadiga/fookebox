5c198cb5446292ed49aa363baac5c830a7d29e44
Stefan Ott <stefan@ott.net>
2008-12-01 04:15:03 +0000

    Moved defaults line
    git-svn-id: https://fookebox.googlecode.com/svn/trunk@33 89ff2e19-6c4f-0410-8d71-0b7837f2a35e

--- config/config.inc.php
+++ config/config.inc.php
@@ -25,9 +25,9 @@
 	if (is_file ($dir . '/site.conf.php'))
 	{
 		require_once ($dir . '/site.conf.php');
+		require_once ($dir . '/defaults.inc.php');
 		define ('smarty_template_dir', $base . '/skins/' . skin .'/templates');
 		require_once ($dir . '/../src/libdesire/config/libdesire.inc.php');
-		require_once ($dir . '/defaults.inc.php');
 	}
 	else
 	{
