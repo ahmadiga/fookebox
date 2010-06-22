0dab32794fd3cb10cdf83ebc9824ddc80561987c
Stefan Ott <stefan@ott.net>
2010-06-22 05:11:31 +0000

    git-svn-id: https://fookebox.googlecode.com/svn/trunk@148 89ff2e19-6c4f-0410-8d71-0b7837f2a35e

--- fookebox/setup.py
+++ fookebox/setup.py
@@ -8,18 +8,24 @@ except ImportError:
 setup(
     name='fookebox',
     version='0.5.0',
-    description='',
+    description='A jukebox-style web-frontend to mpd',
+    long_description="""fookebox is a jukebox-style web-frontend to mpd.
+
+It can be used as a keyboard-less jukebox, as a powerful
+mpd control frontend or as anything in between.""",
     author='Stefan Ott',
     author_email='stefan@ott.net',
     url='http://fookebox.googlecode.com/',
     install_requires=[
         "Pylons>=0.10",
     ],
+    keywords='mpd jukebox web music party',
     setup_requires=["PasteScript>=1.6.3"],
     packages=find_packages(exclude=['ez_setup']),
     include_package_data=True,
     test_suite='nose.collector',
     package_data={'fookebox': ['i18n/*/LC_MESSAGES/*.mo']},
+    license='GPLv3',
     #message_extractors={'fookebox': [
     #        ('**.py', 'python', None),
     #        ('templates/**.mako', 'mako', {'input_encoding': 'utf-8'}),
