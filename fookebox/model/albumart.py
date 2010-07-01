5d4598823aca318e3cbf8d0e0c9ee50e86cf0031
Stefan Ott <stefan@ott.net>
2010-07-01 02:29:29 +0000

    Added support for rhythmbox cover art file naming scheme
    git-svn-id: https://fookebox.googlecode.com/svn/trunk@180 89ff2e19-6c4f-0410-8d71-0b7837f2a35e

--- fookebox/model/albumart.py
+++ fookebox/model/albumart.py
@@ -27,39 +27,18 @@ from pylons import config
 
 log = logging.getLogger(__name__)
 
-class Lock(object):
-
-	class __impl:
-
-		def __init__(self):
-			self.semaphore = BoundedSemaphore(value=1)
-
-		def acquire(self):
-			return self.semaphore.acquire(False)
-
-		def release(self):
-			return self.semaphore.release()
-
-	__instance = None
-
-	def __init__(self):
-		if Lock.__instance is None:
-			Lock.__instance = Lock.__impl()
-
-		self.__dict__['_Lock__instance'] = Lock.__instance
-
-	def __getattr__(self, attr):
-		return getattr(self.__instance, attr)
-
 class Album(object):
 
 	def __init__(self, artist, albumName, disc=None):
-		self.artist = str(artist)
-
 		if albumName == None:
 			self.name = ''
 		else:
 			self.name = str(albumName)
+
+		if artist == None:
+			self.artist = ''
+		else:
+			self.artist = str(artist)
 
 		self.disc = disc
 		self.tracks = []
@@ -70,158 +49,50 @@ class Album(object):
 	def hasCover(self):
 		return self.getCover() != None
 
-	def getCover(self):
+	def _getRockboxPath(self, compilation=False):
 		pattern = re.compile('[\/:<>\?*|]')
+		album = pattern.sub('_', self.name)
 
-		if self.name == None:
-			album = ''
-		else:
-			album = pattern.sub('_', self.name)
-
-		if self.artist == None:
-			artist = ''
+		if compilation:
+			artist = pattern.sub('_', config.get(
+				'compliations_name'))
 		else:
 			artist = pattern.sub('_', self.artist)
 
-		path = '%s/%s-%s.jpg' % (config.get('album_cover_path'),
+		return "%s/%s-%s.jpg" % (config.get('album_cover_path'),
 				artist, album)
 
-		if not os.path.exists(path):
-			path = '%s/%s-%s.jpg' % (config.get('album_cover_path'),
-					config.get('compliations_name'), album)
+	def _getRhythmboxPath(self, compilation=False):
+		album = self.name.replace('/', '-')
 
-			if not os.path.exists(path):
-				return None
+		if compilation:
+			artist = config.get('compliations_name').replace(
+					'/', '-')
+		else:
+			artist = self.artist.replace('/', '-')
 
-		return path
+		return "%s/%s - %s.jpg" % (config.get('album_cover_path'),
+				artist, album)
+
+	def getCover(self):
+		path = self._getRockboxPath()
+		if os.path.exists(path):
+			return path
+
+		path = self._getRockboxPath(True)
+		if os.path.exists(path):
+			return path
+
+		path = self._getRhythmboxPath()
+		if os.path.exists(path):
+			return path
+
+		path = self._getRhythmboxPath(True)
+		if os.path.exists(path):
+			return path
+
+		return None
 
 	def getCoverURI(self):
 		return "%s/%s" % (base64.urlsafe_b64encode(self.artist),
 				base64.urlsafe_b64encode(self.name))
-
-class Genre(object):
-
-	def __init__(self, name):
-		self.name = name
-		self.base64 = base64.urlsafe_b64encode(name)
-
-class Artist(object):
-
-	def __init__(self, name):
-		self.name = name
-		self.base64 = base64.urlsafe_b64encode(name)
-
-class Track(object):
-
-	artist = 'Unknown artist'
-	title = 'Unnamed track'
-	album = None
-	track = 0
-	file = ''
-	b64 = ''
-	disc = 0
-	queuePosition = 0
-
-	def load(self, song):
-		if 'artist' in song:
-			self.artist = song['artist']
-		if 'title' in song:
-			self.title = song['title']
-		if 'file' in song:
-			self.file = song['file']
-			self.b64 = base64.urlsafe_b64encode(self.file)
-		if 'track' in song:
-			# possible formats:
-			#  - '12'
-			#  - '12/21'
-			#  - ['12', '21']
-			t = song['track']
-			if '/' in t:
-				tnum = t.split('/')[0]
-				self.track = int(tnum)
-			elif isinstance(t, list):
-				self.track = int(t[0])
-			else:
-				self.track = int(t)
-		if 'disc' in song:
-			self.disc = song['disc']
-		if 'album' in song:
-			self.album = str(song['album'])
-		if 'pos' in song:
-			self.queuePosition = int(song['pos'])
-
-class MPDWorker(object):
-
-	def __init__(self, num):
-		self.num = num
-		host = config.get('mpd_host')
-		port = config.get('mpd_port')
-		password = config.get('mpd_pass')
-
-		self.mpd = mpd.MPDClient()
-		self.mpd.connect(host, port)
-
-		if password:
-			self.mpd.password(password)
-
-		self.atime = datetime.now()
-		self.free = True
-
-	def __str__(self):
-		return "MPDWorker %d (last used: %s)" % (self.num, self.atime)
-
-	def grab(self):
-		self.free = False
-		self.atime = datetime.now()
-
-	def release(self):
-		self.atime = datetime.now()
-		self.free = True
-		log.debug("Worker %s released" % self)
-
-	def __getattr__(self, attr):
-		self.atime = datetime.now()
-		return getattr(self.mpd, attr)
-
-class MPDPool(object):
-
-	lock = None
-
-	def __init__(self):
-		self.lock = BoundedSemaphore(value=1)
-		self._workers = []
-
-	def getWorker(self):
-		self.lock.acquire()
-
-		log.debug("Pool contains %d workers" % len(self._workers))
-		for worker in self._workers:
-			if worker.free:
-				log.debug("Re-using worker %s" % worker)
-				worker.grab()
-				self._cleanup()
-				self.lock.release()
-				return worker
-
-		try:
-			worker = MPDWorker(len(self._workers))
-		except Exception:
-			self.lock.release()
-			log.fatal('Could not connect to MPD')
-			raise
-
-		log.debug("Created new worker %s" % worker)
-		worker.grab()
-		self._workers.append(worker)
-		self.lock.release()
-		return worker
-
-	def _cleanup(self):
-		now = datetime.now()
-
-		for worker in self._workers:
-			if worker.free:
-				if (now - worker.atime).seconds > 10:
-					log.debug("Removing idle worker %s" %
-							worker)
-					self._workers.remove(worker)
