5480477fbc868fe8b550646c47863f8ff78f8760
Stefan Ott <stefan@ott.net>
2010-07-01 02:45:46 +0000

    Updated license for fookebox.js
    git-svn-id: https://fookebox.googlecode.com/svn/trunk@183 89ff2e19-6c4f-0410-8d71-0b7837f2a35e

--- fookebox/public/js/fookebox.js
+++ fookebox/public/js/fookebox.js
@@ -1,11 +1,10 @@
 /*
- * fookebox 0.5.0-PRE
- * Copyright (C) 2007-2010 Stefan Ott. All rights reserved.
+ * fookebox 0.5.0, http://fookebox.googlecode.com/
  *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,8 +13,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * $Id: fookebox.js 135 2010-06-16 01:38:35Z stefan@ott.net $
  */
 
 // the currently open tab
